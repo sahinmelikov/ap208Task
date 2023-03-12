3d0c5f09ed83528f10fb40d885fe20996ee75369
sahinmelikov <1rima621melikov@gmail.com>
2023-03-13 00:48:55 +0400

    c# and js task add

---
+++ ap208Task/ap208Task/Program.cs
@@ -0,0 +1,51 @@
+﻿
+/*int eded = 1234;
+
+int ters=0;
+
+while (eded > 0)
+{
+   int sonuncu = eded % 10;
+     ters = sonuncu + ters * 10;
+    eded = eded / 10;
+
+}
+Console.WriteLine(ters);*/
+
+/////task2    
+/*long eded = 991111111111;
+long yenieded = eded;
+long say = 10;
+while (eded > 0)
+{
+    say = say * 10;
+    eded = eded / 10;
+
+
+}
+double yeni = (say * 3) / 10 + yenieded;
+Console.WriteLine(yeni);*/
+
+
+
+
+
+///// diger usul;
+
+/*long eded = 12345678999;
+long yenieded = eded;
+long say = 0;
+long ilkeded = 3;
+long yeni = 0;
+while (yenieded > 0)
+{
+    yenieded = yenieded / 10;
+    say++;
+}
+while (say > 0)
+{
+    ilkeded = 10 * ilkeded;
+    say--;
+}
+yeni = ilkeded + eded;
+Console.WriteLine(yeni);*/
